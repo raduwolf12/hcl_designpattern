40185e15c8be988cc83ebccdd6eaa81bb108fdd2
ltudorica <ltudorica@yahoo.com>
2021-11-29 08:02:35 +0200

    last test

--- designpattern/src/main/java/singletonPattern/MainApp.java
+++ designpattern/src/main/java/singletonPattern/MainApp.java
@@ -11,9 +11,13 @@ public class MainApp {
 		System.out.println("Starting thread2: ");
 		t2.start();
 
-		System.out.println(t1.getName());
+		System.out.println("Thread 1 name: " + t1.getName());
 		Thread t3 = Thread1.getInstance();
-		System.out.println(t3.getName());
+		System.out.println("Thread 3 name: " + t3.getName());
+
+		System.out.println("Thread 2 name: " + t2.getName());
+		Thread t4 = Thread2.getInstance();
+		System.out.println("Thread 4 name: " + t4.getName());
 
 	}
 
